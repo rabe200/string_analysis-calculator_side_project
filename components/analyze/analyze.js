e8330ed1f682b4d867998571ce59e9d7f277fc64
rabewtf <rabenklar@gmx.de>
2023-03-05 07:13:24 +0100

    add handleoperators

--- components/analyze/analyze.js
+++ components/analyze/analyze.js
@@ -1,7 +1,7 @@
 import { operator } from "../../index.js"
 import { operatorIndexInString } from "../../index.js"
 import { string }from "../../index.js"
-import { number } from "../../index.js"
+import { numbers } from "../../index.js"
 
 export const analyzeArray = () => {
   console.log('operatorIndexInString: ', operatorIndexInString, 'operator: ', operator)
@@ -13,20 +13,20 @@ export const analyzeArray = () => {
     if (i<1) {
     console.log('---0')
     console.log(string.slice(i, operatorIndexInString[i]));
-    number[i] = string.slice(i, operatorIndexInString[i]);
-    console.log(number[i]);
+    numbers[i] = string.slice(i, operatorIndexInString[i]);
+    console.log(numbers[i]);
   } else if (i<2 ) {
     console.log('---1')
     let operatorIndexInStringPlusOne = operatorIndexInString[i-1] + 1;
-    number[i] = string.slice(operatorIndexInStringPlusOne, operatorIndexInString[i]);
-    console.log('numberArray of i', number[i]);
+    numbers[i] = string.slice(operatorIndexInStringPlusOne, operatorIndexInString[i]);
+    console.log('numberArray of i', numbers[i]);
     console.log('i',i);
   } else if (i>=2) {
     console.log('---2')
     let operatorIndexInStringPlusOne = operatorIndexInString[i-1] + 1;
-    number[i] = string.slice(operatorIndexInStringPlusOne, operatorIndexInString[i]);
-    console.log(number[i]);
+    numbers[i] = string.slice(operatorIndexInStringPlusOne, operatorIndexInString[i]);
+    console.log(numbers[i]);
     console.log('i', i, 'operator.length', operator.length);
     }}
-  console.log(number);
-  }
+
+}
